828f406163a6f5e011e7d3ef1aaaf0baa380191f
Tijme <tijmevh@gmail.com>
2024-10-13 21:27:44 +0200

    minor changes

--- .config/waybar/powermenu.sh
+++ .config/waybar/powermenu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CHOICE=$(echo -e "Logout\nReboot\nShutdown" | wofi --dmenu --width 200 --height 150)
+# CHOICE=$(echo -e "Logout\nReboot\nShutdown" | rofi -show dmenu --width 200 --height 150)
+CHOICE=$(echo -e "Logout\nReboot\nShutdown" | rofi -dmenu -p "System Menu" -normal-window)
 
 case "$CHOICE" in
     Logout)
